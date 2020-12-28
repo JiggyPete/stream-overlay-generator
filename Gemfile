140bf2d0a26e0bbc32d05e680d35e54c2ae13b7a
jiggypete <jiggypete@gmail.com>
2020-12-28 19:09:36 +0000

    Removing everything

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source 'https://rubygems.org'
-
-gem "filewatcher", "~> 1.1.1"
--- watch.rb
+++
@@ -1,7 +0,0 @@
-require 'filewatcher'
-
-Filewatcher.new(["generate-site.rb", "config.yml", "templates/", "javascript/", "images/"]).watch do |changes|
-  puts "Updated: #{changes}"
-  puts ""
-  `ruby generate-site.rb`
-end
