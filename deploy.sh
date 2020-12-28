250e99d90800f8dd4015e5500aaee87fd20c9dc6
jiggypete <jiggypete@gmail.com>
2020-12-28 23:09:18 +0000

    Adding logging to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
-ruby generate-site.rb ENV=production
-
+echo "Clearing temp-generated-stream-overlay"
 rm -rf ../temp-generated-stream-overlay
 mkdir ../temp-generated-stream-overlay
 
+ruby generate-site.rb ENV=production
+
+echo "Moving production code to temp-generated-stream-overlay"
 cp -a generated/. ../temp-generated-stream-overlay
+
+echo "checking out static-site"
 git checkout static-site
 
+echo "Deleting everything in static-site branch"
 rm *.*
 rm Gemfile
 rm -rf generated
@@ -19,9 +24,12 @@ rm -rf slides-overlay/
 rm -rf speaker-with-slides-overlay/
 rm -rf two-speakers-overlay/
 
+echo "copying back from temp-generated-stream-overlay"
 cp -a ../temp-generated-stream-overlay/. .
+
 git add .
 git commit -m 'New updates'
 git push origin static-site
 
+echo "checking out main"
 git checkout main
